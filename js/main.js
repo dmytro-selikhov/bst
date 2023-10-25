f2ce99dd49f7fdf7b0d81c2f1a89694fb2e89e26
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2023-10-25 16:08:08 +0300

    slider Swiper is adding to 2nd section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
 // alert('script is working!');
+
+// new Swiper('.image-slider');
+
+const swiper = new Swiper('.image-slider', {
+  // // Optional parameters
+  // direction: 'vertical',
+  // loop: true,
+
+  // // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+
+  // // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
