7f0f0c43d8cd81ed15e1bad1d177d44ed60b1cbc
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2023-10-25 18:30:24 +0300

    slider Swiper is adding to 2nd section in progress

--- js/main.js
+++ js/main.js
@@ -7,19 +7,65 @@ const swiper = new Swiper('.image-slider', {
   // direction: 'vertical',
   // loop: true,
 
-  // // If we need pagination
-  // pagination: {
-  //   el: '.swiper-pagination',
-  // },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    /*
+    // Bullets
+    clickable: true,
+    // Dynamic Bullets 
+    dynamicBullets: true,
+    // Custom bullets
+    // renderBullet: function (index, className) {
+    //   return '<span class="' + className + '">' + (index + 1) + '</span>';
+    // },
+    */
 
-  // // Navigation arrows
+    // Fraction
+    type: 'fraction',
+    // Custom fraction
+    renderFraction: function (currentClass, totalClass) {
+      return 'Photo <span class="' + currentClass + '"></span>' + ' of ' + '<span class="' + totalClass + '"></span>';
+    },
+
+    /*
+    // Progressbar
+    type: 'progressbar'
+     */
+  },
+
+  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 
-  // // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    // Possibility to drag
+    draggable: true,
+  },
+
+
+
+
+  // Switch on or switch off draggable on PC
+  simulateTouch: true,
+  // Cursor sensitivity
+  touchRatio: 1,
+  // Angle of swipe operation - dragging
+  touchAngel: 45,
+  // Drag cursor
+  grabCursor: true,
+
+  // Switch when clicking on slide
+  slideToClickedSlide: true,
+
+  // Hash navigation
+  hashNavigation: {
+    // Watch state
+    watchState: true,
+  }
+
 });
