728ef6ae3174ba2edae2fdcc8f80295eb214d5c5
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2023-10-29 22:40:18 +0200

    added second test slider

--- js/script.js
+++ js/script.js
@@ -27,4 +27,32 @@ $(document).ready(function () {
       }
     ]
   });
+  // $('.slider2').slick({
+  //   arrows: false,
+  //   dots: false,
+  //   slidesToShow: 1,
+  //   autoplay: false,
+  //   speed: 1000,
+  //   autoplaySpeed: 800,
+  //   adaptiveHeight: false,
+  //   easing: 'liner',
+  //   infinite: false,
+  //   pauseOnFocus: true,
+  //   pauseOnHover: true,
+  //   pauseOnDotsHover: true,
+  //   responsive: [
+  //     {
+  //       breakpoint: 768,
+  //       settings: {
+  //         slidesToShow: 1
+  //       }
+  //     },
+  //     {
+  //       breakpoint: 550,
+  //       settings: {
+  //         slidesToShow: 1
+  //       }
+  //     }
+  //   ]
+  // });
 });
