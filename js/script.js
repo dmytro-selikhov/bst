92478c484f055f032d2e3244f5a58947ca94b81e
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2023-10-31 18:38:31 +0200

    Added sliderThree and base for quizTwo

--- js/script.js
+++ js/script.js
@@ -56,6 +56,35 @@ $(document).ready(function () {
     ]
   });
 
+  $('.sliderThree').slick({
+    arrows: true,
+    dots: true,
+    slidesToShow: 1,
+    autoplay: false,
+    speed: 1000,
+    autoplaySpeed: 800,
+    adaptiveHeight: false,
+    easing: 'liner',
+    infinite: false,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
 
 
 });
